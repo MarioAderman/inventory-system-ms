3845d917a954ac5883ac2a28bfa58dc71bf70c49
MarioAderman <mario.aderman@gmail.com>
2025-04-30 13:13:27 -0600

    Added .env access for Vercel deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // If run locally
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api', // If run locally
 });
 
 // Endpoints for products table
